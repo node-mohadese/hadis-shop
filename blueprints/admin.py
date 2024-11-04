21345e563fb04f5d9e6e7c250dbc863a02f33415
mohaddes <mohaddesenode@gmai.com>
2024-11-04 15:57:59 +0330

    admin and user blueprints added

---
+++ blueprints/admin.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+app = Blueprint("admin", __name__)
+
+
+@app.route('/admin')
+def admin():  # put application's code here
+    return 'this is admin page!'
+
--- blueprints/general.py
+++ blueprints/general.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 app = Blueprint("general", __name__)
 
+
 @app.route('/')
 def main():  # put application's code here
     return 'this is main page!'
+
 
 @app.route('/about')
 def about():  # put application's code here
