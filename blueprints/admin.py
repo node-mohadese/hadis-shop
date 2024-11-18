0e21483cd23856e42f1b7e5db1e45ee196298fe3
mohaddes <mohaddesenode@gmai.com>
2024-11-18 12:11:21 +0330

    image for products

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -43,6 +43,7 @@ def products():
         description = request.form.get('description', None)
         price = request.form.get('price', None)
         active = request.form.get('active', None)
+        file = request.files.get('cover', None)
 
     p = Product(name=name, description=description, price=price)
     if active == None:
@@ -53,7 +54,10 @@ def products():
     db.session.add(p)
     db.session.commit()
 
+    file.save(f'static/cover/{p.id}.jpg')
+
     return "done"
+
 
 @app.route('/admin/dashboard/edit-product/<id>', methods=["GET", "POST"])
 def edit_product(id):
@@ -65,6 +69,7 @@ def edit_product(id):
         description = request.form.get('description', None)
         price = request.form.get('price', None)
         active = request.form.get('active', None)
+        file = request.files.get('cover', None)
 
         product.name = name
         product.description = description
@@ -76,9 +81,7 @@ def edit_product(id):
 
         db.session.commit()
 
+        if file != None :
+            file.save(f'static/cover/{product.id}.jpg')
+
         return redirect(url_for("admin.edit_product", id=id))
-
-
-
-
-
