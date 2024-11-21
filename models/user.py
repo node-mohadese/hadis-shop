473c9696cfc2fe68158f1fdcc761c1400b6f9c90
mohaddes <mohaddesenode@gmai.com>
2024-11-21 17:19:34 +0330

    user dashboard added

--- models/user.py
+++ models/user.py
@@ -1,11 +1,12 @@
-
 from sqlalchemy import *
 from extentions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(Integer, primary_key=True)
-    username = db.Column(String, unique=True, nullable=False ,index=True)
-    password = db.Column(String , nullable=False ,index=True)
-    phone = db.Column(String(11), nullable=False,index=True)
-    address = db.Column(String, nullable=False,index=True)
+    username = db.Column(String, unique=True, nullable=False, index=True)
+    password = db.Column(String, nullable=False, index=True)
+    phone = db.Column(String(11), nullable=False, index=True)
+    address = db.Column(String, nullable=False, index=True)
