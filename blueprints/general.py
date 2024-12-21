64477f300197a20c38de4564cd4e37a711c50085
mohaddes <mohaddesenode@gmai.com>
2024-12-22 01:49:49 +0330

    better templates,search products added

--- blueprints/general.py
+++ blueprints/general.py
@@ -1,19 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models.product import Product
 
 app = Blueprint("general", __name__)
 
 
 @app.route('/')
-def main():  # put application's c
-    products = Product.query.filter(Product.active==1).all()
-    return render_template('main.html', products=products)
+def main():
+    search = request.args.get('search', None)
+    products = Product.query.filter(Product.active == 1)
+    if search != None:
+        products = products.filter(Product.name.like(f'%{search}%'))
+
+    products = products.all()
+    return render_template('main.html', products=products, search=search)
 
 
 @app.route('/product/<int:id>/<name>')
 def product(id, name):
-    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(Product.active == 1).first_or_404()
-    return render_template('product.html',product=product)
+    product = Product.query.filter(Product.id == id).filter(Product.name == name).filter(
+        Product.active == 1).first_or_404()
+    return render_template('product.html', product=product)
 
 
 @app.route('/about')
